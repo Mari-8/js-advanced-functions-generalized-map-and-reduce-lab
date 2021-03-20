02c5a4b582128b8899dde300308fd7dbc22f66c3
Mari-8 <ryanaltimari8@gmail.com>
2021-03-19 23:45:42 -0500

    commit tree 89ad29355efcb671ab41daa0e5223a3e6a3b5e0a

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Add your functions here
+
+const map = function(array, func) {
+    let newA = [] 
+    for (let i=0; i < array.length; i++) {
+        newA.push(func(array[i]))
+    }
+    return newA
+}
